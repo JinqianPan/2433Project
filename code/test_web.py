578d362a927da60996db411671ccc92e4c94d8f1
JinqianPan <60722953+JinqianPan@users.noreply.github.com>
2022-12-18 16:02:25 -0500

    Update test_web.py

--- code/test_web.py
+++ code/test_web.py
@@ -8,14 +8,17 @@ table_names = python_connect_mysql(config, state='show_tables')
 app = Flask(__name__)
 @app.route('/')
 def p1():
+   # redirect to initial_page
    return redirect(url_for('initial_page'))
 
 @app.route('/initial_page')
 def initial_page():
+   # Show tables
    return render_template('p1.html', table_names=table_names)
 
 @app.route('/table', methods=['POST', 'GET'])
 def table():
+   # Show a table of data from database
    if request.method == 'POST':
       result = request.form
       table_name = result['table_name']
@@ -26,13 +29,16 @@ def table():
 @app.route('/operation', methods=['POST', 'GET'])
 def operation():
    if request.method == 'GET':
+      # Go back to initial_page
       return redirect(url_for('initial_page'))
    else:
+      # Go to the operation page
       result = request.form
       table_name = result['table_name']
-      operation_name = result['operation']
       col_names = python_connect_mysql(config, state='show_col', table_name=table_name)
       data = python_connect_mysql(config, table_name=table_name)
+      operation_name = result['operation']
+
       if operation_name == 'insert':
          return render_template("insert.html", table_name=table_name, col_names=col_names, data=data, operation_name=operation_name)
       elif operation_name == 'delete':
@@ -43,16 +49,20 @@ def operation():
 @app.route('/success', methods=['POST', 'GET'])
 def operation_toSQL():
    if request.method == 'POST':
+      # Do insert, delete and update
       result = request.form
       table_name = result['table_name']
       col_names = python_connect_mysql(config, state='show_col', table_name=table_name)
       data = python_connect_mysql(config, table_name=table_name)
+
       query = operation_to_sql(config, col_names=col_names, input_value=result, data=data, operation=result["operation_name"])
       return render_template("operat_info.html", result=result, query=query)
 
 @app.route('/success/1', methods=['POST', 'GET'])
 def succ():
+   # Show the infomation of operations
    if request.method == 'GET':
+      # Go back to initial_page
       return redirect(url_for('initial_page'))
 
 if __name__ == '__main__':
